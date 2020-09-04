3faf1e584e22a3ce9bf9ab0c01b9d999cea79c43
gumi <bmazur85@gmail.com>
2020-09-04 14:11:24 +0200

    testing ecto

--- lib/appTest_web/controllers/page_controller.ex
+++ lib/appTest_web/controllers/page_controller.ex
@@ -1,7 +1,12 @@
 defmodule AppTestWeb.PageController do
   use AppTestWeb, :controller
 
+  alias AppTest.News.Link
+
   def index(conn, _params) do
-    render(conn, "index.html")
+    %Link{url: "http://add.pl", description: "desc2"} |> AppTest.Repo.insert
+
+    result = AppTest.Repo.get(Link, 2).url
+    render(conn, "index.html", link: result)
   end
 end
